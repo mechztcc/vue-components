bd12174c51d1d989e02b229752fe39c3c05202be
mechztcc <dojathan@gmail.com>
2022-07-04 08:52:35 -0400

    feat: using slot to communycate

---
+++ src/components/Citacao.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="citacao">
+        <slot name="autor"></slot>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['texto']
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
